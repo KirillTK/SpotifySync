bfdacc48240d1cb8b25f31721d0cc50444323a4b
KirillTK <kirill99.tkachev@yandex.by>
2020-03-16 00:36:53 +0300

    Display tracks

--- front/src/store/selectors.ts
+++ front/src/store/selectors.ts
@@ -1,5 +1,15 @@
 import { User } from 'interfaces/User';
 import { RootReducer } from 'reducers';
+import { SearchSongsList, SongsReducer } from 'reducers/songs-reducer';
+
+// USER
 
 export const getUser = (state: RootReducer): User | undefined =>
   state.profile.user;
+
+// SPOTIFY
+
+export const getSongsState = (state: RootReducer): SongsReducer => state.songs;
+
+export const getSearchTrackList = (state: RootReducer): SearchSongsList =>
+  getSongsState(state).searchSongsList;
